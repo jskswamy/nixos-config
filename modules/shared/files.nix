452d09b47bef99ec5ed23b7f0416aa1c8adf588c
Krishnaswamy Subramanian <jskswamy@gmail.com>
2025-08-18 03:45:59 +0530

    Add tmux configuration files and update home-manager
    This commit introduces a local tmux configuration file sourced from
    gpakosz's oh-my-tmux repository, enhancing the customization of
    tmux sessions.
    
    Additionally, the home-manager configuration is updated to disable
    the previous tmux plugin setup and instead utilize the new
    configuration files, streamlining user experience and
    maintaining consistency in tmux settings.

--- modules/shared/files.nix
+++ modules/shared/files.nix
@@ -1,4 +1,18 @@
 { pkgs, config, ... }:
-
+let
+   ohMyTmux = pkgs.fetchFromGitHub {
+      owner = "gpakosz";
+      repo = "oh-my-tmux";
+      rev = "master";
+      sha256 = "sha256-ynbmBELDhuFxXWExcOuy73uumsZanB8rAvK3/lalHQ0=";
+    };
+in
 {
+  ".tmux.conf" = {
+    source = "${ohMyTmux}/.tmux.conf";
+  };
+
+  ".tmux.conf.local" = {
+    source = ./config/tmux/conf.local;
+  };
 }
